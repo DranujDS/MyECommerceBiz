374c3784789640f45230b2070777fae7b18c323e
junardds <junardds@gmail.com>
2020-05-16 18:25:50 +0800

    Finished Basket Service

---
+++ MyBiz/MyBiz.Core/Contracts/IBasketService.cs
@@ -0,0 +1,18 @@
+﻿using MyBiz.Core.ViewModels;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Web;
+
+namespace MyBiz.Core.Contracts
+{
+  public interface IBasketService
+  {
+    void AddtoBasket( HttpContextBase httpContext, string productId );
+    void RemovefromBasket( HttpContextBase httpContext, string itemId );
+    List<BasketItemViewModel> GetBasketItems( HttpContextBase httpContext );
+    BasketSummaryViewModel GetBasketSummary( HttpContextBase httpContext );
+  }
+}
